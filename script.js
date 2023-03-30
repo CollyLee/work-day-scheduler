11e4e06555e2f3fcb1c226593149fd27d0f82665
CollyLee <clh5160@gmail.com>
2023-03-30 15:37:04 -0400

    added clear button, cleaned up coding comments

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(document).ready(function ()
+$(document).ready(function () {
 
 
 // -------------------------------------------------------------------------------
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
+// Function that changes the color of the hour blocks depending on if they are past, present, or future
 
 var currentHour = parseInt(dayjs().format('HH'))
 console.log(currentHour);
@@ -32,9 +25,7 @@ $('.time-block').each(function () {
 })
 
 // -------------------------------------------------------------------------------
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
+// Function that loads the textarea input saved from the previous session
 
 // evaluates for a falsy statement, gives a blank array if null
 var storageOutput = JSON.parse(localStorage.getItem('hour-tasks')) || []
@@ -46,17 +37,11 @@ $('.description').each(function (indexItem) {
 })
 
 // ------------------------------------------------------------------------------
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
+// Button that saves the textarea
 
 var storageArray = []
 
 function saveTasks() {
-
   storageArray = []
 
   $('.description').each(function () {
@@ -69,7 +54,7 @@ function saveTasks() {
 $('.saveBtn').on('click', saveTasks)
 
 // ------------------------------------------------------------------------------
-// TODO: Add code to display the current date in the header of the page.
+// Displays current date in the header
 
 var currentTimeHeader = $('#current-time')
 setInterval(myTimer, 1000);
@@ -78,3 +63,18 @@ function myTimer() {
   const date = new Date();
   document.getElementById("current-time").innerHTML = "The current time is " + dayjs().format('MMM D, YYYY, HH:MM:ss A')
 }
+
+// ------------------------------------------------------------------------------
+// Button that clears the localStorage
+
+var clearButton = document.getElementById('clear-button')
+
+function clearCal() {
+  localStorage.removeItem('hour-tasks');
+  location.reload();
+}
+
+clearButton.addEventListener('click', clearCal);
+
+})
+
